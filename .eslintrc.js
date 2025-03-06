ebcf2f5dc634afa2219a8f1b28e9ecf842254ca6
Gabriel Soares Maciel <gsm2015@Outlook.com.br>
2025-03-05 21:37:52 -0300

    feat: configure lint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,22 @@
 // https://docs.expo.dev/guides/using-eslint/
 module.exports = {
-  extends: 'expo',
+  extends: ['expo', '@rocketseat/eslint-config/react'],
+  plugins: ['prettier'],
   ignorePatterns: ['/dist/*'],
-};
+  ignorePatterns: ['.eslintrc.js'],
+  parser: '@typescript-eslint/parser',
+  rules: {
+    'prettier/prettier': 'error',
+    'react/display-name': 'off',
+    'prefer-regex-literals': 'off',
+    'no-useless-escape': 'off',
+    'no-labels': 'off',
+    '@typescript-eslint/no-non-null-assertion': 'off',
+    '@typescript-eslint/no-unused-vars': 'warn',
+    '@typescript-eslint/no-empty-function': 'off',
+    '@typescript-eslint/no-non-null-asserted-optional-chain': 'off',
+    '@typescript-eslint/ban-ts-comment': 'off',
+    'react-hooks/exhaustive-deps': 'off',
+    'react-hooks/rules-of-hooks': 'off',
+  },
+}
