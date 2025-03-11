5fd53e9c5a4f87bc2047296522ab8f7c5939f5e7
Gabriel Soares Maciel <gsm2015@Outlook.com.br>
2025-03-11 15:55:43 -0300

    feat: remove android folder

--- src/providers/root-provider/index.tsx
+++ src/providers/root-provider/index.tsx
@@ -3,10 +3,20 @@ import { ThemeProvider } from 'styled-components/native'
 import { theme } from '@constants'
 import type { RootProviderProps } from '@providers'
 import Toast from 'react-native-toast-message'
+import { useAuthStore } from '@store'
+import { useEffect } from 'react'
+import { useNavigation } from '@hooks'
+import { PublicScreens, PrivateScreens } from '@enums'
 
 const queryClient = new QueryClient()
 
 export function RootProvider({ children }: RootProviderProps) {
+  const { replace } = useNavigation()
+  const { isAuthenticated } = useAuthStore()
+
+  useEffect(() => {
+    replace(isAuthenticated() ? PrivateScreens.HOME : PublicScreens.LOGIN)
+  }, [isAuthenticated])
   return (
     <ThemeProvider theme={theme}>
       <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
