0b0d5def1794007543c0e1d6d55ef20514ca9e4c
Gabriel Soares Maciel <gsm2015@Outlook.com.br>
2025-03-12 20:35:11 -0300

    fix: env variables

--- src/services/apis/api.ts
+++ src/services/apis/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
-import { EXPO_PUBLIC_API_URL } from '@constants'
+// import { EXPO_PUBLIC_API_URL } from '@constants'
 
 export const api = axios.create({
-  baseURL: EXPO_PUBLIC_API_URL,
+  baseURL: process.env.EXPO_PUBLIC_API_URL,
   timeout: 15000, // fifteen seconds
 })
