d2529f1f1c8e247831e42d05648cc8a058641ff5
Gabriel Soares Maciel <gsm2015@Outlook.com.br>
2025-03-14 14:15:00 -0300

    feat: add env

--- src/services/apis/api.ts
+++ src/services/apis/api.ts
@@ -3,6 +3,6 @@ import axios from 'axios'
 const envTest = process.env
 console.log(envTest)
 export const api = axios.create({
-  baseURL: envTest.EXPO_PUBLIC_API_URL,
+  baseURL: process.env.EXPO_PUBLIC_API_URL,
   timeout: 15000, // fifteen seconds
 })
